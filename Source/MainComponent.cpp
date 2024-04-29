3fe72734d4e1bcd214625a9fdb06141bed9f57ec
1ChilleHotdogPro <diederik.deutman@gmail.com>
2024-04-30 01:25:33 +0200

    Font 100 -> 90

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,7 +16,7 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (100.0f));
+    g.setFont (juce::Font (90.0f));
     g.setColour (juce::Colours::white);
     g.drawText ("I love CP+!!!!!", getLocalBounds(), juce::Justification::centred, true);
 }
